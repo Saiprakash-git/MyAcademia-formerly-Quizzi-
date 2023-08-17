bf39d73e3859750ec68e084e54d07048830cc769
Sai_prakash <120245826+Saiprakash-git@users.noreply.github.com>
2023-08-17 21:14:30 +0530

    Add files via upload
    yayy. Good to go . Keep goinggggg

---
+++ quiz/quiz/models.py
@@ -0,0 +1,12 @@
+from quiz import db
+from flask_login import UserMixin 
+
+class User(db.Model, UserMixin): 
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False) 
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    role = db.Column(db.String(10), nullable=False)
+    password = db.Column(db.String(60), nullable=False)
+    
+    def __repr__(self): 
+        return f"User('{self.username}','{self.email}','{self.role}')"
