b2ea6893f0a1b15fd34dfb2d79e555de30cc6a43
Sai_prakash <120245826+Saiprakash-git@users.noreply.github.com>
2024-12-24 00:04:27 +0530

    Update

--- quiz/run.py
+++ quiz/run.py
@@ -1,8 +1,8 @@
 from quiz import app, socketio
-
+import os
 
 if __name__ == '__main__':
    
    socketio.run(app,allow_unsafe_werkzeug=True)
-
-   
+   port = int(os.environ.get("PORT", 5000))  # Default to 5000 if no port is provided
+   app.run(host="0.0.0.0", port=port)
